256368e40ee4830364b180fa909b2e05225dffbf
Ayxan Mustafayev <118119951+ayxansumqayit@users.noreply.github.com>
2025-01-11 23:50:57 +0400

    migrations+model

--- news/models.py
+++ news/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="photo/%Y/%m/%d/")
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+    
+    def __str__(self):
+        return self.title
